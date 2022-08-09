d202a1a41fd2b2ec0bf8a6829a2da8f3623e61e2
AgbemorGrace <gagbemor3@gmail.com>
2022-08-09 14:05:27 +0000

    first commit

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+export const Button1 = ()=> {
+    return <button>Add to Cart</button>;
+};
+export const Button2 = ()=> {
+    return <button>login</button>;
+};
+export const Button3 = ()=> {
+    return <button>Rigister</button>;
+};
+export const Button4 = ()=> {
+    return <button>Signup</button>;
+};
---
+++ src/components/image.jsx
@@ -0,0 +1,4 @@
+import image from "../assets/img/cock.png"
+export const Image = ()=> {
+    return <img src ={image} alt ="" />
+};
